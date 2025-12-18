30557785eedf59c31e0f0e8a9a0010da2048e2dd
almac2022 <al@newgraphenvironment.com>
2025-12-18 12:44:58 -0800

    all work to send in chunks by hand

--- scripts/xmas_2025.R
+++ scripts/xmas_2025.R
@@ -36,13 +36,16 @@ magick::image_write(img_with_quote, path = "fig/peace_2025_quote.png")
 # build the email body
 email_body <- blastula::add_image(
   file = 'fig/peace_2025_quote.png',
-  width = "75%"
+  width = "75%",
+  # read by screen readers if images don’t load
+  alt = "Holiday card: Peace in space with nine moons"
 )
 
 # include the logo
 logo_footer <- blastula::add_image(
   file = "fig/nge-full_black.png",
-  width = "25%"
+  width = "25%",
+  alt = "New Graph Logo"
 )
 
 email <- blastula::compose_email(
@@ -97,7 +100,8 @@ l <- c(
   # 'tara@newgraphenvironment.com',
   # 'mateo@newgraphenvironment.com',
   'al@newgraphenvironment.com',
-  'info@newgraphenvironment.com')
+  'info@newgraphenvironment.com'
+  )
 
 # grab a list of all the contacts as the contacts_all object
 source("~/Projects/current/Admin/contacts/contacts.R")
@@ -114,12 +118,58 @@ email_send_batch <- function(recip){
   )
 }
 
+l <- c(
+  'scottfinlayson@cordilleraconsulting.ca',
+  'Brent.Murray@unbc.ca',
+  'Caitlin.Pitt@unbc.ca',
+  'alex.bevington@unbc.ca',
+  'mfjeld@nupqu.com'
+)
+
 # test locally
 l |>
   purrr::map(email_send_batch)
 
 
 # and go ahead and mail it to the whole list
-contacts_all |>
-  purrr::map(email_send_batch)
+# contacts_all |>
+#   dplyr::pull(email) |>
+#   purrr::map(email_send_batch)
 
+# break into batches of 100 too send and not hit limits
+contacts_split <- split(contacts_all$email, ceiling(seq_along(contacts_all$email) / 100))
+
+
+# because we hit the limit at 159 (send another c3) we restart at 162
+contacts_remaining <- contacts_all |>
+  dplyr::slice(330:nrow(contacts_all))
+# 185
+# contacts_split <- split(contacts_remaining$email, ceiling(seq_along(contacts_remaining$email) / 25))
+#
+# # send the emails in batches every 30 seconds
+# contacts_split |>
+#   purrr::walk(\(x) {
+#     email_send_batch(x)
+#     Sys.sleep(120)
+#   })
+#
+# purrr::walk(seq_along(contacts_split), \(i) {
+#   message("Sending batch ", i, " of ", length(contacts_split))
+#   email_send_batch(contacts_split[[i]])
+#   Sys.sleep(120)
+# })
+
+# rather than mess with throttleing and limits just send to yourself and bcc everyone
+contacts_remaining_list <- contacts_remaining |>
+  dplyr::pull(email)
+
+blastula::smtp_send(
+  email = email,
+  from = "al@newgraphenvironment.com",
+  to = "al@newgraphenvironment.com",
+  bcc = contacts_remaining_list[1:70],
+  subject = "Happy Holidays from New Graph Environment!",
+  credentials = blastula::creds_key(id = "gmail")
+)
+
+c("james.morgan@gov.bc.ca", "jamesbaxternelson@gmail.com")
